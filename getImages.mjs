72a623dd1adad2679d85c5b2a71f514c2d7a096e
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-06-02 04:37:15 +0700

    don't use id

--- getImages.mjs
+++ getImages.mjs
@@ -2,19 +2,16 @@ import fs from "node:fs/promises";
 import { glob } from "glob";
 import { getPlaiceholder } from "plaiceholder";
 
-let id = 0;
 const getImages = async (pattern) => {
-  const files = glob.sync(pattern);
   const images = await Promise.all(
-    files.map(async (file) => {
+    glob.sync(pattern).map(async (file) => {
       const src = file.replace("images\\", "");
-      const buffer = await fs.readFile(file);
       const {
         metadata: { height, width },
       } = await getPlaiceholder(file);
+      const buffer = await fs.readFile(file);
       const { base64 } = await getPlaiceholder(buffer);
       return {
-        id: ++id,
         src,
         width,
         height,
@@ -26,5 +23,14 @@ const getImages = async (pattern) => {
 };
 
 const images = await getImages("images/*.{jpg,png}");
+images.sort((a, b) => {
+  if (a.src < b.src) {
+    return -1;
+  }
+  if (a.src > b.src) {
+    return 1;
+  }
+  return 0;
+});
 
 fs.writeFile("images.json", JSON.stringify(images, null, 2));
