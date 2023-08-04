ea4e0e6d287fb3a252272b4ba5a4da1f634e493c
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-08-04 22:49:34 +0700

    v23.8.4

--- src/getImages.mjs
+++ src/getImages.mjs
@@ -3,30 +3,20 @@ import { glob } from 'glob';
 import { getPlaiceholder } from 'plaiceholder';
 
 export async function getImages(pattern) {
-  const images = await Promise.all(
-    glob.sync(pattern, { posix: true }).map(async (file) => {
-      const src = file.replace('images/', '');
-      const buffer = await fs.readFile(file);
-      const {
-        metadata: { height, width },
-      } = await getPlaiceholder(buffer);
-      const { base64 } = await getPlaiceholder(buffer);
-      return {
-        src,
-        width,
-        height,
-        base64,
-      };
-    })
-  );
-  images.sort((a, b) => {
-    if (a.src < b.src) {
-      return -1;
-    }
-    if (a.src > b.src) {
-      return 1;
-    }
-    return 0;
+  const files = glob.sync(pattern, { posix: true });
+  const imagePromises = files.map(async (file) => {
+    const src = file.replace('images/', '');
+    const buffer = await fs.readFile(file);
+    const {
+      metadata: { height, width },
+      base64,
+    } = await getPlaiceholder(buffer);
+    return { src, width, height, base64 };
   });
+
+  const images = await Promise.all(imagePromises);
+
+  images.sort((a, b) => a.src.localeCompare(b.src));
+
   return images;
 }
