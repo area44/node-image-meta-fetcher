d3ea78e4fc8311d88811edab0e40ff0b877974ad
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-08-05 23:33:44 +0700

    v23.8.4

--- src/getImages.mjs
+++ src/getImages.mjs
@@ -1,22 +1,27 @@
-import fs from 'node:fs/promises';
+import { promises as fs } from 'fs';
 import { glob } from 'glob';
 import { getPlaiceholder } from 'plaiceholder';
 
 export async function getImages(pattern) {
-  const files = glob.sync(pattern, { posix: true });
-  const imagePromises = files.map(async (file) => {
-    const src = file.replace('images/', '');
-    const buffer = await fs.readFile(file);
-    const {
-      metadata: { height, width },
-      base64,
-    } = await getPlaiceholder(buffer);
-    return { src, width, height, base64 };
-  });
+  try {
+    const files = glob.sync(pattern, { posix: true });
+    const imagePromises = files.map(async (file) => {
+      const src = file.replace(/^.*[\\\/]/, '');
+      const buffer = await fs.readFile(file);
+      const {
+        metadata: { height, width },
+        base64,
+      } = await getPlaiceholder(buffer);
+      return { src, width, height, base64 };
+    });
 
-  const images = await Promise.all(imagePromises);
+    const images = await Promise.all(imagePromises);
 
-  images.sort((a, b) => a.src.localeCompare(b.src));
+    images.sort((a, b) => a.src.localeCompare(b.src));
 
-  return images;
+    return images;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
 }
--- tests/getImages.test.mjs
+++ tests/getImages.test.mjs
@@ -4,9 +4,9 @@ import { getImages } from '../src/getImages.mjs';
 
 const images = await getImages('images/*.{jpg,png}');
 
-test('function returns an array of images', async () => {
+test('getImages should return an array of images', () => {
   assert.type(images, 'object');
-  assert.is(images.length, 2);
+  assert.equal(images.length, 2);
 
   assert.type(images[0].src, 'string');
   assert.type(images[0].width, 'number');
@@ -14,25 +14,25 @@ test('function returns an array of images', async () => {
   assert.type(images[0].base64, 'string');
 });
 
-test('function sorts images alphabetically', async () => {
-  assert.is(images[0].src, 'eveling-salazar-TqikiXaDrf4-unsplash.jpg');
-  assert.is(images[1].src, 'mo-ZLBmpXhbzMk-unsplash.jpg');
+test('getImages should sort images alphabetically', () => {
+  assert.equal(images[0].src, 'eveling-salazar-TqikiXaDrf4-unsplash.jpg');
+  assert.equal(images[1].src, 'mo-ZLBmpXhbzMk-unsplash.jpg');
 });
 
-test('function gets width and height of images', async () => {
-  assert.is(images[0].width, 480);
-  assert.is(images[0].height, 600);
+test('getImages should get width and height of images', () => {
+  assert.equal(images[0].width, 480);
+  assert.equal(images[0].height, 600);
 
-  assert.is(images[1].width, 480);
-  assert.is(images[1].height, 613);
+  assert.equal(images[1].width, 480);
+  assert.equal(images[1].height, 613);
 });
 
-test('function gets base64 of images', async () => {
-  assert.is(
+test('getImages should get base64 of images', () => {
+  assert.equal(
     images[0].base64,
     'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAMAAAAECAIAAADETxJQAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAMUlEQVR4nGP48+TZnJL82cW5DAuampNM9fdP7WNQYWCwEBK6vnM7Q0FAQK6P7/+vXwFwfBHUaTcNcAAAAABJRU5ErkJggg=='
   );
-  assert.is(
+  assert.equal(
     images[1].base64,
     'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAMAAAAECAIAAADETxJQAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAM0lEQVR4nAEoANf/AKjg/6zf/7jr/wDt//+0vsPq5+QALjFWNDZOa15vAAAANAAFOgACOPlHEtuYfOsCAAAAAElFTkSuQmCC'
   );
